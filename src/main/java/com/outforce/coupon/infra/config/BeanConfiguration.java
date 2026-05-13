0e86a5c57cc4ed01c4ff60cd54e44a55c2383bb0
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-13 18:58:22 -0300

    feat: Configura o Bean que dá contexto ao usecase de criar cupom

---
+++ src/main/java/com/outforce/coupon/infra/config/BeanConfiguration.java
@@ -0,0 +1,15 @@
+package com.outforce.coupon.infra.config;
+
+import com.outforce.coupon.core.gateways.CouponGateway;
+import com.outforce.coupon.core.usecases.CreateCouponCase;
+import com.outforce.coupon.core.usecases.CreateCouponCaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public CreateCouponCase createCouponCase(CouponGateway couponGateway){
+        return new CreateCouponCaseImpl(couponGateway);
+    }
+}
