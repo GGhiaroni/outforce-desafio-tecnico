48d1f8d675e089c992d1522873a089b5ac1955c0
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-13 16:56:36 -0300

    test: Refatora o teste da excecão gerada por um cupom not found

--- src/test/java/com/outforce/coupon/core/exceptions/CouponNotFoundExceptionTest.java
+++ src/test/java/com/outforce/coupon/core/exceptions/CouponNotFoundExceptionTest.java
@@ -1,15 +1,17 @@
 package com.outforce.coupon.core.exceptions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("CouponNotFoundException")
 class CouponNotFoundExceptionTest {
 
     @Test
-    void shouldCreateExceptionWithCorrectMessage() {
-        CouponNotFoundException exception =
-                new CouponNotFoundException(10L);
+    @DisplayName("Cria a exceção com a mensagem formatada incluindo o id")
+    void criaExcecaoComMensagemFormatada() {
+        CouponNotFoundException exception = new CouponNotFoundException(10L);
 
         assertEquals(
                 "Coupon with id 10 not found",
