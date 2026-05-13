1f98ce2a45bf1962f33f6e1ecdadcb436112052c
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-12 21:07:45 -0300

    chore: bootstrap projeto Spring Boot 3 com Maven, JPA, H2 e Validation

---
+++ src/main/java/com/outforce/desafio_tenico/DesafioTenicoApplication.java
@@ -0,0 +1,13 @@
+package com.outforce.desafio_tenico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTenicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTenicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outforce/desafio_tenico/DesafioTenicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outforce.desafio_tenico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTenicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
